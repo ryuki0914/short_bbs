d2a54051dba7b050609f1ca06f2baad4ed06dab6
ryuki0914 <ryuki20040915@icloud.com>
2025-06-06 13:35:02 +0900

    lolipop_1

--- view.php
+++ view.php
@@ -1,15 +1,12 @@
 <?php
     session_start();
 
-    $host = 'localhost';
-    $dbname = 'bbs';          
-    $user = 'root';          
-    $pass = 'root';   
-
     try {
     // DSN（データソース名）を作成し、PDOオブジェクトで接続
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo=new PDO('mysql:host=mysql305.phy.lolipop.lan;
+    dbname=LAA1553908-php2024;charset=utf8mb4',
+    'LAA1553908',
+    'Pass0914');
 
     // エラーモードを「例外」に設定（エラー時に例外が発生するようにする）
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
