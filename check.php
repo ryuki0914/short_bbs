d7652c5540e4e9052a053f18f4cfab86f70f4531
kawate0928 <2301151@s.asojuku.ac.jp>
2025-06-06 10:58:34 +0900

    テスト

---
+++ check.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+// SQL接続に必要な情報を変数に格納
+$host = 'localhost';
+$dbname = 'bbs';          
+$user = 'root';          
+$pass = 'root';   
+
+try {
+    // DSN（データソース名）を作成し、PDOオブジェクトで接続
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $pass);
+
+    // エラーモードを「例外」に設定（エラー時に例外が発生するようにする）
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "接続成功<br>";
+
+} catch (PDOException $e) {
+    // エラーが発生した場合の処理
+    echo "接続失敗: " . $e->getMessage();
+}
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+
+            $sql = $pdo->prepare("SELECT * FROM user WHERE password = ? AND username = ?");
+
+            if($sql){
+                $sql->bindParam(2, $username);
+                $sql->bindParam(1, $hashedPassword);
+                $test = $sql->execute();
+            }
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($user){
+                //ログイン成功
+                $_SESSION['username'] = $user->username;   
+                $_SESSION['password'] = $user->password;  
+                 header("Location: form.php");
+                 exit;
+            }else{
+                //ログイン失敗 
+                 header("Location: login.php");
+                 exit;
+            }
+        
+
+    
+
+
+?>
